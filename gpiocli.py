2ca2e79000fa3d65f9b4d980d02bc30c374cef22
Jeroen Versteeg <jversteeg@gmail.com>
2012-12-23 12:14:50 +0100

    Extended GPICLI with no-cleanup, verbose options By default GPICLI calls GPIO.cleanup on exit, which can be prevented using the -n|--no-cleanup option The -v|--verbose adds some logging

--- gpiocli.py
+++ gpiocli.py
@@ -4,27 +4,40 @@ import RPi.GPIO as GPIO
 import sys
 
 class GPIOCLI:
-	def __init__(self, pinId, quiet=False):
+	def __init__(self, pinId, cleanUpOnExit=True, verbose=False, quiet=False):
 		self.pinId = pinId
+		self.cleanUp = cleanUpOnExit
+		self.verbose = verbose
 		if quiet:
 			GPIO.setwarnings(False)
 		GPIO.setmode(GPIO.BOARD)
 		GPIO.setup(pinId, GPIO.OUT)
 
 	def __del__(self):
-		GPIO.cleanup()
+		if self.cleanUp:
+			if self.verbose:
+				print "  GPIO cleanup"
+			GPIOCLI.cleanup()
 
 	def setOutLow(self):
+		if self.verbose:
+			print "  GPIO set {pinId} LOW".format(pinId=pinId)
 		GPIO.output(self.pinId, GPIO.LOW)
 
 	def setOutHigh(self):
+		if self.verbose:
+			print "  GPIO set {pinId} HIGH".format(pinId=pinId)
 		GPIO.output(self.pinId, GPIO.HIGH)
+
+	@staticmethod
+	def cleanup():
+		GPIO.cleanup()
 
 def printHelp():
 	print """
 	GPIO-CLI usage:
 		gpiocli.py	cleanup
-		gpiocli.py	PinID (HIGH|LOW) [-q]
+		gpiocli.py	PinID (HIGH|LOW) [-n|--no-cleanup] [-q|--quiet] [-v|--verbose]
 	"""
 
 if __name__ == '__main__':
@@ -35,24 +48,39 @@ if __name__ == '__main__':
 
 	# handle cleanup
 	if sys.argv[1] == "cleanup":
-		GPIO.cleanup()
+		GPIOCLI.cleanup()
 		exit()
 	elif len(sys.argv) < 2:
 		printHelp()
 		exit()
 
-	# set pin id
-	gpiocli = GPIOCLI(
-		int(sys.argv[1]),							# PinID
-		len(sys.argv) == 4 and sys.argv[3] == "-q"  # optional -q flag
-	)
-
+	# gather parameters
+	pinId = int(sys.argv[1])
 	setting = sys.argv[2].lower()
-	if setting == "high":
-		gpiocli.setOutHigh()
-	elif setting == "low":
-		gpiocli.setOutLow()
-	else:
-		print "Invalid setting"
+	if not(setting == "high" or setting == "low"):
+		print "Invalid setting:", setting
 		printHelp()
 		exit(1)
+
+	cleanUp = True
+	verbose = False
+	quiet = False
+	for arg in sys.argv[3:]:
+		if arg == "-n" or arg == "--no-cleanup":
+			cleanUp = False
+		elif arg == "-q" or arg == "--quiet":
+			quiet = True
+		elif arg == "-v" or arg == "--verbose":
+			verbose = True
+		else:
+			print "invalid option:", arg
+			printHelp()
+			exit(1)
+
+	# set up GPIO and set pin
+	gpiocli = GPIOCLI(pinId, cleanUp, verbose, quiet)
+
+	if setting == "high":
+		gpiocli.setOutHigh()
+	else:
+		gpiocli.setOutLow()
